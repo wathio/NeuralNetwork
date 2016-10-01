c4adbbd33d81f61dfac98bd4d72a6da418f20fd0
Thierry Wambo <jean.oscar@yahoo.fr>
2016-10-01 11:43:31 -0500

    Update ex4.m

--- ex4.m
+++ ex4.m
@@ -1,33 +1,15 @@
-%% Machine Learning Online Class - Exercise 4 Neural Network Learning
-
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions 
-%  in this exericse:
-%
-%     sigmoidGradient.m
-%     randInitializeWeights.m
-%     nnCostFunction.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
+%% Machine Learning: Neural Network Learning
 
 %% Initialization
 clear ; close all; clc
 
-%% Setup the parameters you will use for this exercise
+%% Setup the parameters 
 input_layer_size  = 400;  % 20x20 Input Images of Digits
 hidden_layer_size = 25;   % 25 hidden units
 num_labels = 10;          % 10 labels, from 1 to 10   
                           % (note that we have mapped "0" to label 10)
 
-%% =========== Part 1: Loading and Visualizing Data =============
-%  We start the exercise by first loading and visualizing the dataset. 
-%  You will be working with a dataset that contains handwritten digits.
-%
+%% =========== Part 1: Loading and Visualizing Data set=============
 
 % Load Training Data
 fprintf('Loading and Visualizing Data ...\n')
@@ -46,7 +28,7 @@ pause;
 
 
 %% ================ Part 2: Loading Parameters ================
-% In this part of the exercise, we load some pre-initialized 
+% loading some pre-initialized 
 % neural network parameters.
 
 fprintf('\nLoading Saved Neural Network Parameters ...\n')
@@ -57,18 +39,9 @@ load('ex4weights.mat');
 % Unroll parameters 
 nn_params = [Theta1(:) ; Theta2(:)];
 
-%% ================ Part 3: Compute Cost (Feedforward) ================
-%  To the neural network, you should first start by implementing the
-%  feedforward part of the neural network that returns the cost only. You
-%  should complete the code in nnCostFunction.m to return cost. After
-%  implementing the feedforward to compute the cost, you can verify that
-%  your implementation is correct by verifying that you get the same cost
-%  as us for the fixed debugging parameters.
-%
-%  We suggest implementing the feedforward cost *without* regularization
-%  first so that it will be easier for you to debug. Later, in part 4, you
-%  will get to implement the regularized cost.
-%
+%% ================ Part 3: Compute Cost function (Feedforward) ================
+
+
 fprintf('\nFeedforward Using Neural Network ...\n')
 
 % Weight regularization parameter (we set this to 0 here).
@@ -83,10 +56,8 @@ fprintf(['Cost at parameters (loaded from ex4weights): %f '...
 fprintf('\nProgram paused. Press enter to continue.\n');
 pause;
 
-%% =============== Part 4: Implement Regularization ===============
-%  Once your cost function implementation is correct, you should now
-%  continue to implement the regularization with the cost.
-%
+%% =============== Part 4: Implement Regularization for neural network ===============
+
 
 fprintf('\nChecking Cost Function (w/ Regularization) ... \n')
 
@@ -104,9 +75,8 @@ pause;
 
 
 %% ================ Part 5: Sigmoid Gradient  ================
-%  Before you start implementing the neural network, you will first
-%  implement the gradient for the sigmoid function. You should complete the
-%  code in the sigmoidGradient.m file.
+%  implement the gradient for the sigmoid function. 
+%  call for sigmoidGradient.m 
 %
 
 fprintf('\nEvaluating sigmoid gradient...\n')
@@ -121,10 +91,8 @@ pause;
 
 
 %% ================ Part 6: Initializing Pameters ================
-%  In this part of the exercise, you will be starting to implment a two
-%  layer neural network that classifies digits. You will start by
-%  implementing a function to initialize the weights of the neural network
-%  (randInitializeWeights.m)
+% initialize the weights of the neural network
+%  call for randInitializeWeights.m
 
 fprintf('\nInitializing Neural Network Parameters ...\n')
 
@@ -136,11 +104,7 @@ initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
 
 
 %% =============== Part 7: Implement Backpropagation ===============
-%  Once your cost matches up with ours, you should proceed to implement the
-%  backpropagation algorithm for the neural network. You should add to the
-%  code you've written in nnCostFunction.m to return the partial
-%  derivatives of the parameters.
-%
+
 fprintf('\nChecking Backpropagation... \n');
 
 %  Check gradients by running checkNNGradients
@@ -151,9 +115,8 @@ pause;
 
 
 %% =============== Part 8: Implement Regularization ===============
-%  Once your backpropagation implementation is correct, you should now
-%  continue to implement the regularization with the cost and gradient.
-%
+%  implement the regularization with the cost and gradient.
+
 
 fprintf('\nChecking Backpropagation (w/ Regularization) ... \n')
 
@@ -173,19 +136,14 @@ pause;
 
 
 %% =================== Part 8: Training NN ===================
-%  You have now implemented all the code necessary to train a neural 
-%  network. To train your neural network, we will now use "fmincg", which
-%  is a function which works similarly to "fminunc". Recall that these
-%  advanced optimizers are able to train our cost functions efficiently as
-%  long as we provide them with the gradient computations.
-%
+%use "fmincg" to train neural network
+
 fprintf('\nTraining Neural Network... \n')
 
-%  After you have completed the assignment, change the MaxIter to a larger
-%  value to see how more training helps.
+
 options = optimset('MaxIter', 50);
 
-%  You should also try different values of lambda
+%  choosing a value for lambda
 lambda = 1;
 
 % Create "short hand" for the cost function to be minimized
@@ -210,7 +168,7 @@ pause;
 
 
 %% ================= Part 9: Visualize Weights =================
-%  You can now "visualize" what the neural network is learning by 
+%  visualize what the neural network is learning by 
 %  displaying the hidden units to see what features they are capturing in 
 %  the data.
 
@@ -223,9 +181,7 @@ pause;
 
 %% ================= Part 10: Implement Predict =================
 %  After training the neural network, we would like to use it to predict
-%  the labels. You will now implement the "predict" function to use the
-%  neural network to predict the labels of the training set. This lets
-%  you compute the training set accuracy.
+%  the labels. 
 
 pred = predict(Theta1, Theta2, X);
 
